4850b0616a2a5f6ba20d0fef10401a77ec6d5a04
ichad <richard.betalmos@mlhuillier.com>
2025-08-08 13:31:34 +0800

    [feature][richard] mysql db setup

--- apps/crmscore/src/app/crmscore/crmscore.module.ts
+++ apps/crmscore/src/app/crmscore/crmscore.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CrmscoreController } from './crmscore.controller';
 import { CrmscoreService } from './crmscore.service';
+import { DatabaseService } from '../db/db.service';
 
 @Module({
   controllers: [CrmscoreController],
-  providers: [CrmscoreService],
+  providers: [CrmscoreService, DatabaseService],
 })
 export class CrmscoreModule {}
